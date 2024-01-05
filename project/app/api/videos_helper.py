c060fa76635388e632fdc1063a486a9297791f7a
joesurf <desictiverk@gmail.com>
2024-01-05 17:02:43 +0800

    Setup video generation

--- project/app/api/videos_helper.py
+++ project/app/api/videos_helper.py
@@ -1,5 +1,7 @@
 from pydantic import AnyHttpUrl
 
+# TODO: implement auto saving using useeffect with PUT_CONTENT + OpenAI moderation
+
 
 async def generate_video_from_script(script: str) -> AnyHttpUrl:
     return "https:/joesurf.io"
